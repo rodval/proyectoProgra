988e40754b5a6ef37eccb763e9e25f40093a75cb
roDvalM <rodrigovalladaresm1@gmail.com>
2021-10-20 01:19:55 -0600

    que molesto lol

--- js/redireccionController.js
+++ js/redireccionController.js
@@ -1,7 +1,7 @@
 function realizaProceso(datos){
     $.ajax({
         type: "GET",
-        url: "index.php?controlador=paginas&accion=categorias",
+        url: "?controlador=paginas&accion=categorias",
         data: "valor=" + datos,
         success:function(data) {
             document.getElementsByTagName("html")[0].innerHTML = data;
